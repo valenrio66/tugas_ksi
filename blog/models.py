b115b71ef77cd0ff6d054d4f37ffefc5af79b01a
Valen Rionald <vrionald@gmail.com>
2022-12-14 22:46:42 +0700

    Tugas Pertemuan 10

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=25)
+    body = models.TextField()
+    email = models.EmailField(blank=True)
+    slug = models.SlugField(blank=True, editable=False)
+
+    def save(self):
+        self.slug = slugify(self.title)
+        super(Post, self).save()
+
+    def __str__(self):
+        return "{}".format(self.title)
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from multiprocessing import context
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
+    db = Post.objects.all()
     context = {
         'Judul' : 'Blog Saya',
         'h1' : 'About Me',
@@ -13,8 +15,13 @@ def index(request):
         'Email' : 'vrionald@gmail.com',
         'HP' : '089522910966',
         'Umur' : '20',
-        'Desc' : 'Mahasiswa, Jomblo, Tersesat'
+        'Desc' : 'Mahasiswa, Jomblo, Tersesat',
+        'title':'Blog',
+        'heading':'Blog',
+        'subheading':'postingan',
+        'post': db,
     }
+
     return render(request, 'blog/index.html', context)
 
 def recent(request):
